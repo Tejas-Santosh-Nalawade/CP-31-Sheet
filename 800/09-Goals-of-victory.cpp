36398c24f7769445a4acfc70aeb41598979b69ea
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-06-09 23:56:32 +0530

    (DAY 8) 8 JUNE 2025  Completed of 365 DAY CP Challenge Goal of Victory Problem

---
+++ 800/09-Goals-of-victory.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long tt;
+    cin>>tt;
+    while(tt--){
+        long long n;
+        cin>>n;
+        vector<long long> a(n);
+        for (int i = 1; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        long long sum = 0;
+        for(int i=0; i<n;  i++)
+        {
+            sum +=a[i];
+        }
+        cout<<(-sum)<<"\n";
+        
+    }
+    return 0;
+}
