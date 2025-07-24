2f53e3b8d42030122af54b540aaa1c4ee8c81f79
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-07-24 06:46:30 +0530

    (Day 55) 24 JULY 2025 Completed of 365 DAY CP Exciting Bets  Problem

---
+++ 900/24-Exciting-Bets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long tt;
+    cin >> tt;
+    while (tt--)
+    {
+        long long a, b;
+        cin >> a >> b;
+        if (b > a)
+            swap(a, b); 
+
+        if (a == b)
+            cout << 0 << " " << 0 << "\n"; 
+        else
+        {
+            long long gcd = a - b;
+            long long moves = min(b % gcd, gcd - b % gcd); // 1
+            cout << gcd << " " << moves << "\n";
+        }
+    }
+    return 0;
+}
