13706c775642f839a975bb879c9dc750e446c70f
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-10-07 23:20:07 +0530

    (Day 131) 7 October 2025 Completed of 365 DAY CP 1200

---
+++ 1200/7-Playing-in-a-casio.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int tt;
+	cin >> tt; 
+
+	while (tt--)
+	{
+		int n, m;
+		cin >> n >> m;
+
+
+        vector<vector<long long>> v(m, vector<long long>(n));
+
+
+        for (int j = 0; j < n; j++)
+		{
+			for (int i = 0; i < m; i++)
+			{
+				cin >> v[i][j]; 
+                
+			}
+		} 
+        
+
+		for (int i = 0; i < m; i++)
+		{
+			sort(v[i].begin(), v[i].end());
+        }
+
+		long long ans = 0;
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				ans -= (v[i][j] * (n - j - 1));
+				ans += (v[i][j] * j);
+			}
+		} 
+
+		cout << ans << "\n";
+	}
+}
