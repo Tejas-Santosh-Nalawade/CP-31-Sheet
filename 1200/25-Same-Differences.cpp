952bb1edae3f24d908af586f6185e6fc630c66bc
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-10-25 22:18:20 +0530

    (Day 149) 26 October 2025 Completed of 365 DAY CP 1200

---
+++ 1200/25-Same-Differences.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int tt;
+	cin >> tt; 
+
+	while (tt--)
+	{
+		int n;
+		cin >> n; 
+
+		vector<int> v(n); 
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> v[i]; 
+
+			v[i] -= i;
+		}
+
+
+		map<long long, long long> freq; 
+
+		for (int i = 0; i < n; i++) 
+			freq[v[i]]++; /
+
+		long long ans = 0; /
+
+		for (auto &[val, count] : freq)
+		{
+			ans += ((count) * (count - 1)) / 2; 
+		}
+
+		cout << ans << "\n";
+	}
+
+	return 0;
+}
+
