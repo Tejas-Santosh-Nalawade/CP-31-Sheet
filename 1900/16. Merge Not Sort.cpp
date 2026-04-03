34a3c61ace1a7f2dceedb5de4cae020a42744da2
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2026-04-03 23:57:36 +0530

    3  April 2026  1900 Rating DAY 94 (1 Question)

---
+++ 1900/16. Merge Not Sort.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	vector<int> C(2 * N);
+	for (int &x : C) cin >> x;
+
+	vector<vector<int>> chunks;
+	chunks.push_back({C[0]});
+	for (int i = 1; i < 2 * N; i++) {
+		if (C[i] < chunks.back()[0]) {
+			chunks.back().push_back(C[i]);
+		} else {
+			chunks.push_back({C[i]});
+		}
+	}
+
+	int k = (int)chunks.size();
+	vector<int> len(k);
+	for (int i = 0; i < k; i++) len[i] = (int)chunks[i].size();
+
+	vector<vector<bool>> dp(k + 1, vector<bool>(N + 1, false));
+	vector<vector<bool>> take(k + 1, vector<bool>(N + 1, false));
+	
+	dp[0][0] = true;
+	for (int i = 1; i <= k; i++) {
+		for (int s = 0; s <= N; s++) {
+			if (dp[i - 1][s]) dp[i][s] = true;
+			if (s >= len[i - 1] && dp[i - 1][s - len[i - 1]]) {
+				dp[i][s] = true;
+				take[i][s] = true;
+			}
+		}
+	}
+
+	if (!dp[k][N]) {
+		cout << -1 << '\n';
+		return 0;
+	}
+
+	vector<int> choose(k, 0);
+	int s = N;
+	for (int i = k; i >= 1; i--) {
+		if (take[i][s]) {
+			choose[i - 1] = 1;
+			s -= len[i - 1];
+		}
+	}
+
+	vector<int> A, B;
+	for (int i = 0; i < k; i++) {
+		if (choose[i]) {
+			for (int x : chunks[i]) A.push_back(x);
+		} else {
+			for (int x : chunks[i]) B.push_back(x);
+		}
+	}
+
+	for (int x : A) cout << x << ' ';
+	cout << '\n';
+	for (int x : B) cout << x << ' ';
+	cout << '\n';
+}
